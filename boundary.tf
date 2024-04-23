64ee800917dd78a0113fd514f867863e0ac49ea0
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2024-04-23 19:09:12 +0100

    Update boundary.tf

--- boundary.tf
+++ boundary.tf
@@ -1,6 +1,6 @@
 resource "hcp_boundary_cluster" "example" {
   cluster_id = "boundary-cluster-secure-access"
-  username   = var.password_auth_method_login
+  username   = var.password_auth_method_login_name
   password   = var.password_auth_method_password
   tier = "plus"
   maintenance_window_config {
