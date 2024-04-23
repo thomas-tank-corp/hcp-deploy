55b0f51f62ffd6a197b24fd5adb449ae3acd1d79
Tom Harris <tharris@hashicorp.com>
2024-04-23 19:30:14 +0100

    ++

--- vault.tf
+++ vault.tf
@@ -1,7 +1,8 @@
 resource "hcp_vault_cluster" "example" {
   cluster_id = "vault-cluster-secure-access"
   hvn_id     = hcp_hvn.example.hvn_id
-  tier       = "standard_large"
+  tier       = "plus"
+  public_endpoint = true
 }
 
 resource "hcp_vault_cluster_admin_token" "example" {
