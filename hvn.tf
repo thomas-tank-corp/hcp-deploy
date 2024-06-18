8435ebd22fc29da31a14c7cc94012b11bb387775
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2024-06-18 13:15:54 +0100

    Update hvn.tf

--- hvn.tf
+++ hvn.tf
@@ -1,6 +1,6 @@
 resource "hcp_hvn" "vault_hvn" {
-  hvn_id         = "hvn"
+  hvn_id         = "hvn-boundary-secure-access"
   cloud_provider = "aws"
-  region         = "us-west-2"
+  region         = "eu-west-2"
   cidr_block     = "172.25.16.0/20"
 }
